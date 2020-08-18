c39dbafd6a9f87985fab2fc2ff1b8cbcbee9995c
21shield <21netaly@gmail.com>
2020-08-18 17:38:22 -0400

    added remmove button

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :image, :description, :quantity, :category
-  has_many :order_items, only: [:order_id, :item_id]
+ 
 
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :number, :items
+  attributes :id, :number, :items, :order_items
   has_many :order_items
   
   
