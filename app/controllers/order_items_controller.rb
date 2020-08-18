13996db5ba20cd29406300b2b439f69df83ff486
21shield <21netaly@gmail.com>
2020-08-18 18:06:38 -0400

    removed item from backend

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,5 +1,11 @@
 class OrderItemsController < ApplicationController
 
+
+    def show
+        order_item = OrderItem.find(params[:id])
+        render json: order_item
+    end
+
     def index
         order_items = OrderItem.all
         render json: order_items
@@ -7,6 +13,12 @@ class OrderItemsController < ApplicationController
 
     def create  
         order_item = OrderItem.create(order_item_params)
+        render json: order_item
+    end
+
+    def destroy
+        order_item = OrderItem.find(params[:id])
+        order_item.destroy
         render json: order_item
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,10 @@ Rails.application.routes.draw do
   get "/orders/:id", to: "orders#show"
   post "/orders", to: "orders#create"
   
-
+  get "/order_items/:id", to: "order_items#show"
   get "/order_items", to: "order_items#index"
   post '/order_items', to: "order_items#create"
-  
+  delete "/order_items/:id", to: "order_items#destroy"
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
