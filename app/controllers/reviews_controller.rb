b3f351b0372a9616fba7b8b2ee52c7665b3dc1a1
21shield <21netaly@gmail.com>
2020-08-20 00:53:47 -0400

    added something

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+
+    def index
+        reviews = Review.all
+        render json: reviews
+    end
+    
+    def create
+        review = Review.create(reviewParams)
+        render json: review
+    end
+
+    def reviewParams
+        params.permit(:content, :user_id, :item_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   
-  resources :reviews
+  
   resources :items
   # resources :users
   get "/users", to: "users#index"
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   post '/order_items', to: "order_items#create"
   delete "/order_items/:id", to: "order_items#destroy"
 
-
+  post "/reviews", to: "reviews#create"
+  get "/reviews", to: "reviews#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
